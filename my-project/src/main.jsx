b7cd4ba01f21e857ef42f9674fc8debbb840bb72
Biell <gabrielsouza65115@gmail.com>
2024-02-12 10:41:46 -0300

    Adicionando as imagens da page

--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import Topo from './Topo.jsx'
+import Texto from './Texto.jsx'
+import Rodape from './Rodape.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    
+    <Topo />
+    <Texto />
+    <Rodape />
   </React.StrictMode>,
 )
